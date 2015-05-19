43d3c2344966a304c8c54a7c6f4bd366b84146d1
Sara <s.schiesaro@hotmail.it>
2015-05-19 12:58:19 +0200

    primo up

---
+++ clojure-training/src/clojure_training/core.clj
@@ -0,0 +1,12 @@
+(ns clojure-training.core
+  (:require [clojure.java.io :as io])
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (with-open [rdr (io/reader "/home/utente/lu980.txt")]
+    (printf "%s\n" (clojure.string/join "\n-" (line-seq rdr))))
+)
+
+
