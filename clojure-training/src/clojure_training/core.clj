f7665470520dd046f99342ec51e523130565207b
Filippo Poltronieri <filippo.poltronieri@gmail.com>
2015-05-20 12:21:06 +0200

    Start to parsing tsp file

--- clojure-training/src/clojure_training/core.clj
+++ clojure-training/src/clojure_training/core.clj
@@ -1,12 +1,22 @@
 (ns clojure-training.core
   (:require [clojure.java.io :as io])
+  (:require [clojure.string :as str])
   (:gen-class))
 
-(defn -main
+
+(defn parse
+"Parsing bnchmark"
+  [lines]
+  (doseq [line lines](println (str/split  line #"\s")))  
+  
+
+  (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (with-open [rdr (io/reader "/home/utente/lu980.txt")]
-    (printf "%s\n" (clojure.string/join "\n-" (line-seq rdr))))
-)
+  (with-open [rdr (io/reader "./src/clojure_training/lu980.txt")]
+    (parse (line-seq rdr)))
+
+  ))
 
 
+  
