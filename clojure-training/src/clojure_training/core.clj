0d7ca6049604c5cf3232775cdf15c19e7a7507dd
Sara <s.schiesaro@hotmail.it>
2015-05-20 22:19:39 +0200

    pars

--- clojure-training/src/clojure_training/core.clj
+++ clojure-training/src/clojure_training/core.clj
@@ -7,7 +7,8 @@
 
 (def nodeMaps [])
 (def numPossMag)
-(def storeCapacity 600)
+(def storeCapacity)
+(def maxDemand)
 
 
 
@@ -15,11 +16,11 @@
 
 ;;-----------FUNZIONI PARSING-----------------
 (defn initNode 
-"inizializza la struttura dati con i dati del benchmark"
- [seq]
- (def nodes (zipmap [:id :x :y :capacity] [(first seq) (second seq) (last seq) 0]))
- (def nodeMaps (conj nodeMaps nodes))
-)
+  "inizializza la struttura dati con i dati del benchmark"
+  [seq]
+  (def nodes (zipmap [:id :x :y :capacity] [(first seq) (second seq) (last seq) 0]))
+  (def nodeMaps (conj nodeMaps nodes))
+  )
 
 (defn parse
 "Parsing benchmark"
@@ -29,21 +30,35 @@
          
             (if (number? (read-string (first lineSplit)))
               (initNode lineSplit) 
-            )
-                    
+            )        
     ) 
   )
 ) 
+
+(defn parseParam
+  "Parameters parsing from input file"
+  [seq]
+  ;;iterate over the file per line
+  (doseq [line seq]
+    (let [lineSplit (take 2 (str/split line #":"))]
+      (if (= (str/trim(first lineSplit)) (str/trim "STORE CAPACITY"))
+        (def storeCapacity (read-string (second lineSplit))))
+      (if (= (str/trim(first lineSplit)) (str/trim "MAXIMUM DEMAND"))
+        (def maxDemand (read-string (second lineSplit))))
+    )
+  )
+)
 ;;------------------------------------------- 
 
 ;;-----------FUNZIONI INIZIALIZZAZIONE ISTANZA
+
 (defn instanceInit
   " "
   []
   ;; determino una stima del numero minimo di magazzini necessari
   ;;((numero clienti * max richiesta cliente)/ capacitàMag) + rand(0,20)
   (def numPossMag (+ (rand-int 20)   
-                     (quot (* (count nodeMaps) 20) storeCapacity))) 
+                     (quot (* (count nodeMaps) maxDemand) storeCapacity))) 
   
   ;;assegno al primo nodo capacità Q= storeCapacity
   (loop [iter 0]
@@ -57,12 +72,20 @@
 )
 
 
-
-
-;;-----------------------------------
+;;------------------ MAIN -----------
 (defn -main
   "Carica i dati dal benchmark e risolve il problema di location routing"
   [& args]
+  (if (or (empty? args) 
+          (> (count args) 1))
+      ;;set to default values
+      (do (def storeCapacity 600 )
+          (def maxDemand 20)) 
+      ;;read values from input file
+      (with-open [rdr (io/reader (str "./resources/"(first args)))]
+        (parseParam (line-seq rdr)))
+  )
+
   ;;apro il file e lo leggo una riga alla volta, lo passo poi al parser
   ;; per l'inizializzazione della struttura dati che conterrà tutti i nodi
   (with-open [rdr (io/reader "./src/clojure_training/lu980.txt")]
@@ -70,6 +93,8 @@
   
   ;;seleziono i possibili nodi magazzino e assegno i pesi
   (instanceInit)
+
+  (println "Massima capacità: " storeCapacity "\nMassima domanda: "maxDemand)
 
   (println "Numero magazzini tot:" numPossMag)
   (println (first nodeMaps))
