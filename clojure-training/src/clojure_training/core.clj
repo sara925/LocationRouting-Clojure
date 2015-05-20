b9542646a6b802be74a798600e046a6256cda65a
Sara <s.schiesaro@hotmail.it>
2015-05-20 13:45:21 +0200

    parsing file

--- clojure-training/src/clojure_training/core.clj
+++ clojure-training/src/clojure_training/core.clj
@@ -3,26 +3,42 @@
   (:require [clojure.string :as str])
   (:gen-class))
 
+(def nodeMaps [{}])
+
+
+(defn initNode 
+"inizializza la struttura dati con i dati del benchmark"
+ [seq]
+ (def nodes (zipmap [:id :x :y] [(first seq) (second seq) (last seq)]))
+ (def nodeMaps (conj nodeMaps nodes))
+)
 
 (defn parse
-"Parsing bnchmark"
+"Parsing benchmark"
   [lines]
-  (doseq [line lines](println (str/split  line #"\s")))  
+  (doseq [line lines] ;;itero su tutte le stringhe
+    (let [lineSplit (take 3 (str/split  line #"\s"))] 
+         
+            (if (number? (read-string (first lineSplit)))
+              (initNode lineSplit) 
+            )
+                    
+    ) 
+  )
+) 
   
 
-  (defn -main
+(defn -main
   "I don't do a whole lot ... yet."
   [& args]
-<<<<<<< HEAD
-  (with-open [rdr (io/reader "/home/utente/lu980.txt")]
-    (printf "%s\n" (clojure.string/join "n-" (line-seq rdr))))
-)
-=======
+  
   (with-open [rdr (io/reader "./src/clojure_training/lu980.txt")]
     (parse (line-seq rdr)))
+  (println  nodeMaps)
+)
 
-  ))
->>>>>>> f7665470520dd046f99342ec51e523130565207b
+
+
 
 
   
