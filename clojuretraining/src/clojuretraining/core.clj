0ca749a747b89f95232dd14e7e70a53548aee714
sara925 <s.schiesaro@hotmail.it>
2015-06-24 14:04:44 +0200

    m

--- clojuretraining/src/clojuretraining/core.clj
+++ clojuretraining/src/clojuretraining/core.clj
@@ -13,6 +13,7 @@
 (load "instanceinit")
 (load "subsetinit")
 (load "kruskall")
+(load "two-opt")
 (load "pgraph")
  
 ;;---general purpose data structures--
@@ -30,38 +31,10 @@
 (def slotsProbability (reductions + [50 30 15 5]))  ;probabilità delle varie fasce
 ;;----------------------------------------------
 
-(defn getAllStore
-  [setArray]
-  ;;all the store map in the subSetArray
-  (map (fn [x] (get-in x [:store]) ) setArray))
 
-(load "two-opt")
 
-(defn createSubSet 
- [slots, subSet]
- 
- (def tmp subSet) 
- (def freeSpace storeCapacity)
- (def slotsCum (createCumulativeSlots slots));lista di liste cumulative
 
- (loop [iter 0]
-   (let [idx (fish slotsProbability)] 
 
-     (if (not (empty? (get slots idx)))
-       
-       (do 
-         (let [idx2 (fish (nth slotsCum idx)) cl (:cl (get (get slots idx) idx2))] 
-              (if (not (contains? tmp cl)) 
-                (do (def tmp (set/union tmp (into #{} (vector cl))))
-                    (def freeSpace (- freeSpace (:capacity cl)))))
-              ))))
-
-   (if (> freeSpace maxDemand) 
-     (recur (inc iter)))
- )
-
- tmp
-)
 
 (defn find-best-store
   [cl,coll]
@@ -81,26 +54,6 @@
 
 )
 
-(defn assign-to-set
-  [cl]
-  (def prescelto (find-best-store cl stores))
-  (def pSet ((group-by :store subSetArray) prescelto))
-  (def pSet (get pSet (rand-int (count pSet))))
-  (def idx (.indexOf subSetArray pSet))
-
-   (let [foglie (MST-leaf pSet)]
-    (loop [iter 0]
-      (def foglia (nth foglie (rand-int (count foglie))))    
-      (def found  (contains? (reduce set/union (set (getAllSet (remove #(= pSet %) subSetArray)))) foglia))
-      (if found
-        (do
-          (def subSetArray (assoc-in subSetArray [idx :set] (into #{cl} (remove #(= foglia %) (getSet pSet))))) ;sostituisce cl a foglia
-        ))
-
-      (if (and (< iter (- (count foglie) 1)) (not found))
-        (recur (inc iter))))
-  )
-)
 
 (defn compCoverRatio
   [s1]
@@ -115,32 +68,6 @@
 )
 
 
-(defn initSubSetArray
- []
-  (def subSetArray [])
-  (find-border-customers)
-  (doseq [iter (range (count stores))] 
-    
-    (def slots (assignProbability (get stores iter)))
-    (doseq [x (range 3)]
-      ;;costruzione del subset come insieme di mappe clienti e mappa store
-      ;;(def subSet #{ {:store (get stores iter)} {:x 1 :y 2} }) ;TODO aggiungi il set
-      (def subSet #{})
-      (def subSet (createSubSet slots subSet))
-      ;;modifica del subset nella struttura
-      ;{: store nodo_magazzino :set insieme dei nodi del set compreso nodo maggazino}
-      (def subSet {:store (get stores iter) :set subSet})
-      (def subSetArray (conj subSetArray subSet)))    
-  )
-  (def subSetArray (into [] (map #(assoc  %1 :id %2)  subSetArray (range (count subSetArray)))))
-  ;;la struttura è quella voluta
-  ;;definisco una function per utilizzare solo la mappa set(clienti+store nelle varie funzioni)
-  (def notAssigned (set/difference (set clients) (set/intersection  (set clients) (reduce set/union  (getAllSet subSetArray) ))))
-  (println "Clienti non assegnati " (count notAssigned))
-  (if (not (empty? notAssigned))
-    (doseq [cl notAssigned] (assign-to-set cl))  
-  )
-)
 
 (defn calcDemand
   [set]
@@ -183,76 +110,6 @@
   J)
 
 
-(defn fixCapacity
-  [Jin]
-  (def J Jin)
-  (doseq [i (range (count J))]
-    (if (> (calcDemand (get J i)) storeCapacity)
-      (do
-        (def foglie (MST-leaf (get J i)) )
-        (loop [toRemove (- (calcDemand (get J i)) storeCapacity)]
-          (def foglia (nth foglie (rand-int (count foglie))))
-          
-          (def s 
-            (find-best-stores 
-               foglia 
-               (getAllStore (into [] (remove nil? (into [] 
-                                   (map (fn [x] (if (< (+ (calcDemand x) (:capacity foglia)) storeCapacity) x)) J )))))))
-    
-          (def idx (.indexOf J (first (filter #(= (:store %) (first s)) J))))
-          ;;rimozione della foglia
-          (def J (assoc-in J [i :set] (set/difference (:set (get J i)) #{foglia})))
-          ;;aggiunta in quello 
-          (def J (assoc-in J [idx :set] (set/union (:set (get J idx)) #{foglia})))
-          (if (> toRemove 0)
-            (recur (- toRemove (:capacity foglia)))))
-      )))
-  J)
-
-(defn constrGreedySol
-  []
-
-  (initSubSetArray)
-  (def subSetTmp subSetArray)
-
-  (def J [])
-  (loop [iter 0]
-    ;;calcolo pesi
-    (doseq [idx (range (count subSetTmp))]
-      (def subSetTmp (assoc-in subSetTmp [idx :cost] (compSetCost (get subSetTmp idx)))))
-
-    (def subSetTmp (sort-by :cost subSetTmp))
-    (def beta (* 1.3 (:cost (first subSetTmp))))
-    (let [P (filter #(< (:cost %) beta) subSetTmp)
-          pi (nth P (rand-int (count P)))
-          ret (checkOcc pi J)]
-      (if (false? ret)
-        (def J (conj J (get subSetArray (.indexOf subSetArray (first (filter #(= (:id pi) (:id %)) subSetArray))))))
-        (def J (assoc J (.indexOf J (first (filter #(= (:store pi) (:store %)) J))) ret)))
-      
-      (def subSetTmp (into [] (map (fn [x] (assoc x :set (set/difference (:set x) (:set pi)))) subSetTmp)))
-      (def subSetTmp (into [] (remove #(empty? (:set %)) subSetTmp)))
-      
-      ;(println "Rimasti "(count (reduce set/union (getAllSet subSetTmp))))
-      ) 
-    
-    (if (not (empty? (reduce set/union (getAllSet subSetTmp))))
-      (recur (inc iter)))
-    )
-
-  ;"rifinisco" J
-  (def J (into [] (remove #(empty? (:set %))  J)))
-  (def J (remove-duplicates J))
-  (def J (into [] (remove #(empty? (:set %))  J)))
- 
-  (loop [] 
-    (def J (fixCapacity J))
-    (if (some #(> % storeCapacity) (map calcDemand J))
-      (recur)))
-
-  (println (map calcDemand J))
-
-  J)
 
 (defn evaluate
   [in]
